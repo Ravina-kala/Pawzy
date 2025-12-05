99d9314963cceedd58ecceebde884b105d437059
Ravina-kala <ravinakala81@gmail.com>
2025-12-05 18:45:38 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,7 @@ const CACHE_VERSION = "pawzy-v2";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 
+
 const STATIC_ASSETS = [
   "/",
   "/index.html",
